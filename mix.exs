5f18d6295243d51a35a2ecbf41994a9b8b6ebe00
Nathan Lee <nathan@globalphobia.com>
2015-08-29 22:15:05 -0600

    Adding comeonin

--- mix.exs
+++ mix.exs
@@ -34,6 +34,8 @@ defmodule Officetournament.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:comeonin, "~> 1.1"}
+    ]
   end
 end
