d8bb14f80696c77d09ec7f023102071c8cae73d9
Nathan Lee <nathan@globalphobia.com>
2015-09-11 08:44:11 -0600

    Will check username AND password now

--- web/controllers/user_session_controller.ex
+++ web/controllers/user_session_controller.ex
@@ -1,10 +1,10 @@
 defmodule Officetournament.UserSessionController do
   use Officetournament.Web, :controller
+  require Logger
 
   alias Officetournament.Login
   alias Officetournament.User
 
-  plug :find_user
   plug :scrub_params, "login" when action in [:create, :update]
 
   def index(conn, _params) do
@@ -13,20 +13,16 @@ defmodule Officetournament.UserSessionController do
   end
 
   def login(conn, %{"login" => %{"username" => username, "password" => password}}) do
-    # user = Repo.all(from u in User, 
-    #                 where: u.username == ^username,
-    #                 select: u)
-    user = Repo.get_by(User, username: username)
+    user = find_user_and_pass(username, password)
 
     if user do
       conn
       |> put_flash(:info, "Welcome #{user.username}! I like your password #{password}")
-      |> put_session(:username, username)
       |> put_session(:current_user_id, user.id)
       |> redirect(to: user_session_path(conn, :index))
     else
       conn
-      |> put_flash(:error, "Username not found.")
+      |> put_flash(:error, "Username and password not found.")
       |> redirect(to: user_session_path(conn, :index))
     end
   end
@@ -51,7 +47,27 @@ defmodule Officetournament.UserSessionController do
     end
   end
 
-  defp find_user(conn, params) do
-    conn |> assign(:username, "This is a test")
+  defp find_user_and_pass(username, password) do
+    user = Repo.one(from u in User,
+                    where: u.username == ^username,
+                    select: u)
+
+    if !is_nil(user) do
+      Logger.debug "Testing #{user.username}:#{user.password}" 
+    end
+
+    if check_password(user, password) do
+      user
+    else
+      nil
+    end
+  end
+
+  defp check_password(nil, password) do
+    nil
+  end
+
+  defp check_password(user, password) do
+    user.password == password
   end
 end
