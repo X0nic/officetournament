a3dbb17975e47dce4616c584a3e4402558dbdd6e
Nathan Lee <nathan@globalphobia.com>
2015-09-27 20:13:37 -0600

    Make available for all views

--- web/views/user_session_view.ex
+++ web/views/user_session_view.ex
@@ -1,11 +1,3 @@
 defmodule Officetournament.UserSessionView do
   use Officetournament.Web, :view
-
-  def link_to(path, text) do
-    raw "<a href=\"#{path}\">#{text}</a>"
-  end
-
-  def link_to(path, text, %{class: class_name}) do
-    raw "<a class=\"#{class_name}\" href=\"#{path}\">#{text}</a>"
-  end
 end
