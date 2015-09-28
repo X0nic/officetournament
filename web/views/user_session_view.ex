b6b166bf6f368f04ebffae022ed2fd9ec354f0df
Nathan Lee <nathan@globalphobia.com>
2015-09-27 20:06:53 -0600

    Fix images and add link_to helper

--- web/views/user_session_view.ex
+++ web/views/user_session_view.ex
@@ -2,6 +2,10 @@ defmodule Officetournament.UserSessionView do
   use Officetournament.Web, :view
 
   def link_to(path, text) do
-    raw "<a class=\"btn btn--google\" href=\"#{path}\">#{text}</a>"
+    raw "<a href=\"#{path}\">#{text}</a>"
+  end
+
+  def link_to(path, text, %{class: class_name}) do
+    raw "<a class=\"#{class_name}\" href=\"#{path}\">#{text}</a>"
   end
 end
