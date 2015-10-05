cfefecdbb3226c763c6fbfb5935f9980dfd54d0b
Nathan Lee <nathan@globalphobia.com>
2015-10-04 22:55:34 -0600

    I guess phoenix already has a link helper

--- web/web.ex
+++ web/web.ex
@@ -66,14 +66,6 @@ defmodule Officetournament.Web do
           ""
         end
       end
-
-      def link_to(path, text) do
-        raw "<a href=\"#{path}\">#{text}</a>"
-      end
-
-      def link_to(path, text, %{class: class_name}) do
-        raw "<a class=\"#{class_name}\" href=\"#{path}\">#{text}</a>"
-      end
     end
   end
 
