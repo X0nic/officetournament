365f59abd01143c9b89f6df3770e2e9bf8aa02e6
Nathan Lee <nathan@globalphobia.com>
2015-10-21 08:33:20 -0600

    View functions for the win

--- web/views/home_view.ex
+++ web/views/home_view.ex
@@ -1,4 +1,7 @@
 defmodule Officetournament.HomeView do
   use Officetournament.Web, :view
 
+  def render("_header.html", assigns) do
+    Officetournament.SharedView.render("_header.html", assigns)
+  end
 end
--- web/views/league_view.ex
+++ web/views/league_view.ex
@@ -1,3 +1,7 @@
 defmodule Officetournament.LeagueView do
   use Officetournament.Web, :view
+
+  def render("_header.html", assigns) do
+    Officetournament.SharedView.render("_header.html", assigns)
+  end
 end
