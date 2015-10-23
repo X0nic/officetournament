12c4f2a138648fd308f73b9416a1778de9624e31
Nathan Lee <nathan@globalphobia.com>
2015-10-22 22:37:02 -0600

    DRY up headers, but allow for custom inner html

--- web/views/home_view.ex
+++ web/views/home_view.ex
@@ -2,6 +2,7 @@ defmodule Officetournament.HomeView do
   use Officetournament.Web, :view
 
   def render("_header.html", assigns) do
-    Officetournament.SharedView.render("_header.html", assigns)
+    inner_html = render("_header_create.html", assigns)
+    Officetournament.SharedView.render("_header.html", Dict.put(assigns, :inner_html, inner_html))
   end
 end
--- web/views/league_view.ex
+++ web/views/league_view.ex
@@ -2,6 +2,12 @@ defmodule Officetournament.LeagueView do
   use Officetournament.Web, :view
 
   def render("_header.html", assigns) do
-    Officetournament.SharedView.render("_header.html", assigns)
+    inner_html = render("_header_back.html", assigns)
+    Officetournament.SharedView.render("_header.html", Dict.put(assigns, :inner_html, inner_html))
+  end
+
+  def render("header_new.html", assigns) do
+    inner_html = render("_header_new.html", assigns)
+    Officetournament.SharedView.render("_header.html", Dict.put(assigns, :inner_html, inner_html))
   end
 end
