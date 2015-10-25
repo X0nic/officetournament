b0c6d9979166dc5d0ff30bc405ade151a3e5f3ef
Nathan Lee <nathan@globalphobia.com>
2015-10-24 23:28:42 -0600

    Dry up the view a little

--- web/views/league_view.ex
+++ web/views/league_view.ex
@@ -3,11 +3,15 @@ defmodule Officetournament.LeagueView do
 
   def render("_header.html", assigns) do
     inner_html = render("_header_back.html", assigns)
-    Officetournament.SharedView.render("_header.html", Dict.put(assigns, :inner_html, inner_html))
+    render("header.html", Dict.put(assigns, :inner_html, inner_html))
   end
 
   def render("header_new.html", assigns) do
     inner_html = render("_header_new.html", assigns)
-    Officetournament.SharedView.render("_header.html", Dict.put(assigns, :inner_html, inner_html))
+    render("header.html", Dict.put(assigns, :inner_html, inner_html))
+  end
+
+  def render("header.html", assigns) do
+    Officetournament.SharedView.render("_header.html", assigns)
   end
 end
