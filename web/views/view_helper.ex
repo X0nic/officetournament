3707055cb4295421505c3594dd97b200a2367037
Nathan Lee <nathan@globalphobia.com>
2015-11-09 22:12:29 -0700

    Request proper size images from auth provider

--- web/views/view_helper.ex
+++ web/views/view_helper.ex
@@ -3,11 +3,17 @@ defmodule Officetournament.ViewHelper do
 
   alias Officetournament.User
 
-  def avatar_path(%User{avatar_url: avatar_url}), do: avatar_path(avatar_url)
+  def avatar_path(%User{avatar_url: avatar_url, provider: provider}), do: avatar_path(provider, avatar_url)
 
-  def avatar_path(avatar_url) when is_binary(avatar_url) do
+  def avatar_path("github", avatar_url) when is_binary(avatar_url) do
     avatar_url
+    |> String.replace("?v=3", "?v=3&s=180")
   end
 
-  def avatar_path(nil), do: nil
+  def avatar_path("google", avatar_url) when is_binary(avatar_url) do
+    avatar_url
+    |> String.replace("?sz=50", "?sz=180")
+  end
+
+  def avatar_path(_, nil), do: nil
 end
